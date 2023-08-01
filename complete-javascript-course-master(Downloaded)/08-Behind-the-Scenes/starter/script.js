15d3c8a14255858c47747753038eb91cb911871f
akash-chandel-jnp <akashsingh12345@gmail.com>
2023-08-01 20:12:11 +0530

    projects completed  - 1)Guess the number 2) Modal

--- complete-javascript-course-master(Downloaded)/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master(Downloaded)/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+// // console.log(student.calcAge(2023));
+// const student = {
+//   fName: 'Akash',
+//   lName: 'Chandel',
+//   job: 'programmer',
+//   year: 1985,
+
+//   calcAge: function (currYear) {
+//     return currYear - Number(this.year); // this keyword belong to the object and is used when some property of the object is accessed.
+//   },
+// };
+
+// console.log(student.calcAge(2023));
+
+////////////////////////////////////////////////////////////////////////////////////////
+function doubleAge() {
+  const birth_year = 1985;
+  // this is undefined for a normal function call which is not part of object(in case of strict mode) but in sloppy mode it points to the window object .
+  //   return 2 * (2030 - this.doubleAge.birth_year);
+  console.log(this);
+}
+console.log(window.doubleAge.birth_year);
+console.log(doubleAge());
